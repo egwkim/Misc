a12d787c80d9ed3d7cc16c815cb17f4714f8047f
egwkim <35051914+egwkim@users.noreply.github.com>
2022-03-20 22:56:09 +0900

    Add hello-ts
    Hello, Typescript!

---
+++ 2022/03/hello-ts/hello-world.js
@@ -0,0 +1,6 @@
+function hello(name) {
+    console.log(`Hello, ${name}!`);
+}
+const name = 'World';
+hello(name);
+export {};
---
+++ 2022/03/hello-ts/hello-world.ts
@@ -0,0 +1,9 @@
+export {};
+
+function hello(name: string) {
+  console.log(`Hello, ${name}!`);
+}
+
+const name: string = 'World';
+
+hello(name);
