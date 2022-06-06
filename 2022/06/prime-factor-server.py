8a573a5f172621256f47accab77d96356b0f8e2d
egwkim <35051914+egwkim@users.noreply.github.com>
2022-06-06 15:48:35 +0900

    Close connection after sending response

--- 2022/06/prime-factor-server.py
+++ 2022/06/prime-factor-server.py
@@ -54,68 +54,76 @@ def factor(n: int, q: Queue = None):
     return factors
 
 
-def handleConnection(conn, addr):
-    res = ""
-    try:
-        # Parse request
-        recv = conn.recv(4096)
-        print(recv)
-        req = HTTPRequest(recv)
-        if req.error_code != None:
-            res = f"HTTP/1.0 {req.error_code} {req.error_message}\n\n"
-            raise InvalidRequestException(
-                f"{req.error_code} {req.error_message}")
+def processConnection(conn, addr):
+    with conn:
+        res = ""
         try:
-            parsed = urlparse(req.path)
-        except AttributeError:
-            return
-        path = parsed.path
-        query = parse_qs(parsed.query)
-        host = req.headers.get("Host")
-        res = ''
-        if not path == "/":
-            raise NotFoundException("Path not found")
-        if not query:
-            res = f"HTTP/1.0 200 OK\n\nPrime Factorization\nQuery with n=number\nn must be an intege and bigger than one\nExample {host}/?n=10\n"
-            return
-        try:
-            n_list = query['n']
-        except KeyError:
-            raise NotFoundException("Invalid query")
-
-        # Process request
-        factor_list = {}
-        for n in n_list:
+            # Parse request
+            recv = conn.recv(4096)
+            print(recv)
+            req = HTTPRequest(recv)
+            if req.error_code != None:
+                res = f"HTTP/1.0 {req.error_code} {req.error_message}\n\n"
+                raise InvalidRequestException(
+                    f"{req.error_code} {req.error_message}")
             try:
-                n = int(n)
-                if n <= 1:
-                    continue
-                q = Queue()
-                p = Process(target=factor, args=(n, q))
-                p.start()
-                p.join(TIMEOUT)
-                if p.is_alive():
-                    p.terminate()
-                    p.join()
-                if q.empty():
-                    raise TimeoutException("Timeout while prime factorizing")
-                factor_list[str(n)] = q.get()
-            except ValueError:
-                continue
-        if factor_list:
-            res = "HTTP/1.0 200 OK\n\n" + json.dumps(factor_list) + "\n"
-        else:
-            raise NotFoundException("Invalid query")
+                parsed = urlparse(req.path)
+            except AttributeError:
+                return
+            path = parsed.path
+            query = parse_qs(parsed.query)
+            host = req.headers.get("Host")
+            res = ''
+            if not path == "/":
+                raise NotFoundException("Path not found")
+            if not query:
+                res = f"HTTP/1.0 200 OK\n\nPrime Factorization\nQuery with n=number\nn must be an intege and bigger than one\nExample {host}/?n=10\n"
+                return
+            try:
+                n_list = query['n']
+            except KeyError:
+                raise NotFoundException("Invalid query")
 
-    # Handle exceptions and send response
-    except InvalidRequestException as e:
-        print(e)
-    except NotFoundException as e:
-        res = f"HTTP/1.0 404 Not Found\n\n{e}\n"
+            # Process request
+            factor_list = {}
+            for n in n_list:
+                try:
+                    n = int(n)
+                    if n <= 1:
+                        continue
+                    factor_list[str(n)] = factor(n)
+                except ValueError:
+                    continue
+            if factor_list:
+                res = "HTTP/1.0 200 OK\n\n" + json.dumps(factor_list) + "\n"
+            else:
+                raise NotFoundException("Invalid query")
+
+        # Handle exceptions and send response
+        except InvalidRequestException as e:
+            print(e)
+        except NotFoundException as e:
+            res = f"HTTP/1.0 404 Not Found\n\n{e}\n"
+        finally:
+            if res == "":
+                if req.error_code != None:
+                    res = f"HTTP/1.0 {req.error_code} {req.error_message}\n\n"
+                else:
+                    res = "HTTP/1.0 500 Internal Server Error\n\n"
+            conn.sendall(res.encode())
+
+
+def handleConnection(conn, addr):
+    try:
+        p = Process(target=processConnection, args=(conn, addr))
+        p.start()
+        p.join(TIMEOUT)
+        if p.is_alive():
+            p.terminate()
+            p.join()
+            raise TimeoutException("Timeout while processing the request")
     except TimeoutException as e:
-        res = f"HTTP/1.0 503 Service Unavailable\n\n{e}\n"
-    finally:
-        conn.sendall(res.encode())
+        conn.sendall(f"HTTP/1.0 503 Service Unavailable\n\n{e}\n".encode())
 
 
 def main():
@@ -127,7 +135,6 @@ def main():
             conn, addr = s.accept()
             with conn:
                 p = Process(target=handleConnection, args=(conn, addr))
-                p.daemon = False
                 p.start()
 
 
