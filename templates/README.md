363f3dcbe813a8565ec130117ad76a290a7e83d6
egwkim <35051914+egwkim@users.noreply.github.com>
2022-07-17 21:56:23 +0900

    Remove templates

--- templates/README.md
+++
@@ -1,1 +0,0 @@
-# A collection of some html/css templates that I might reuse someday.
--- templates/control-panel/control-panel.js
+++
@@ -1,9 +0,0 @@
-// Add an event listener to all input elements in control panel
-
-const settingInputs = document.querySelectorAll('#control-panel input');
-
-settingInputs.forEach((item) => {
-  item.addEventListener("change", () => {
-    // Do something
-  });
-});
