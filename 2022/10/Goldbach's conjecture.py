1c11a3c401d97faf49b35ed3275223a58cbe862c
egwkim <35051914+egwkim@users.noreply.github.com>
2022-10-20 23:04:30 +0900

    Add Goldbach's conjecture.py

---
+++ 2022/10/Goldbach's conjecture.py
@@ -0,0 +1,43 @@
+import math
+
+
+def calc_primes(n: int) -> list[bool]:
+    """
+    Calculate prime numbers up to n
+
+    Returns:
+        list[bool]: list[i] shows if i is a prime number.
+    """
+    p = [True] * (n + 1)
+    p[0] = False
+    p[1] = False
+
+    for i in range(2, n+1):
+        if p[i]:
+            for j in range(i*i, n+1, i):
+                p[j] = False
+    return p
+
+
+def main():
+    # Input an even number
+    n = int(input())
+    if not n > 0:
+        return
+    if n % 2:
+        return
+
+    # Calculate prime numbers
+    # m = math.floor(math.sqrt(n)) + 1
+    m = n
+    primes = calc_primes(m)
+
+    # Brute force
+    for i, p in enumerate(primes):
+        if p and primes[n - i]:
+            print(i, n-i)
+            return
+
+
+if __name__ == '__main__':
+    main()
