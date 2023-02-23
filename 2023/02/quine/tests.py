82507b644f60f88d7118a26f7dfabc48f25588fe
egwkim <35051914+egwkim@users.noreply.github.com>
2023-02-24 00:44:36 +0900

    Update docs in quine/tests.py

--- 2023/02/quine/tests.py
+++ 2023/02/quine/tests.py
@@ -1,40 +1,26 @@
-import os
+"""This module tests other python quines, and also is a quine itself
 
-desc = """
-This file tests other python quines, and also is a quine itself!
 If all quines work fine, print itself.
 Otherwise, raise an error.
 """
 
+import os
+
 DIR = os.path.dirname(__file__)
 
 
-def generate_quine():
-    """
-    Modifies quine function to work properly.
-    """
-
-
-def quine():
-    """
-    Print everything in this file.
-    """
-    # TODO print itself
-    pass
-
-
 def test_self():
-    """
-    Test if quine() in this file works correctly.
-    """
+    """Test if quine function in this file works correctly"""
     # TODO test itself
     pass
 
 
+def test_single_quine(filepath: str):
+    """Test given quine"""
+    
+
 def test_other_python_quines():
-    """
-    Test all quines without this file.
-    """
+    """Test all quines excluding this file"""
     files = [f for f in os.listdir(DIR)]
 
     for f in files:
@@ -43,6 +29,17 @@ def test_other_python_quines():
         if "python" in f or f.split(".")[-1] == "py":
             # TODO Test python code
             pass
+
+
+def generate_quine():
+    """Modify quine function to work properly"""
+    # TODO create quine generator
+    pass
+
+
+def quine():
+    """Print everything in this file (quine/tests.py)"""
+    pass
 
 
 def main():
