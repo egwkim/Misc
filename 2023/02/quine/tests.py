8250f34ba62c869fb580c6202652f8ccd9688779
egwkim <35051914+egwkim@users.noreply.github.com>
2023-02-24 02:07:45 +0900

    Add quine unit tests

--- 2023/02/quine/tests.py
+++ 2023/02/quine/tests.py
@@ -4,31 +4,52 @@ If all quines work fine, print itself.
 Otherwise, raise an error.
 """
 
+import contextlib
+import io
 import os
+import subprocess
+import unittest
+import unittest.mock
 
 DIR = os.path.dirname(__file__)
+EXCLUDE = ["tests.py"]
 
 
-def test_self():
-    """Test if quine function in this file works correctly"""
-    # TODO test itself
-    pass
-
-
-def test_single_quine(filepath: str):
-    """Test given quine"""
-    
-
-def test_other_python_quines():
-    """Test all quines excluding this file"""
-    files = [f for f in os.listdir(DIR)]
-
-    for f in files:
-        if not os.path.isfile(os.path.join(DIR, f)):
-            continue
-        if "python" in f or f.split(".")[-1] == "py":
-            # TODO Test python code
+class TestQuines(unittest.TestCase):
+    def test_self(self):
+        """Test if quine function in this file works correctly"""
+        output = io.StringIO()
+        with contextlib.redirect_stdout(output):
+            quine()
+        with io.open(__file__, encoding="utf-8") as fileIO:
+            # self.assertEqual(output.getvalue(), fileIO.read())
             pass
+
+    def test_other_python_quines(self):
+        """Test all quines excluding this file"""
+        files = [f for f in os.listdir(DIR)]
+
+        for filename in files:
+            filepath = os.path.join(DIR, filename)
+            if not os.path.isfile(filepath):
+                continue
+            if filename in EXCLUDE:
+                continue
+
+            if "python" in filename or filename.split(".")[-1] == "py":
+                with self.subTest(filepath=filepath):
+                    with io.open(filepath, encoding="utf-8") as fileIO:
+                        output = (
+                            subprocess.check_output(["python", filepath])
+                            .decode()
+                            .replace("\r", "")
+                        )
+                        self.assertEqual(output, fileIO.read())
+
+
+def reset_quine():
+    """Reset quine function"""
+    pass
 
 
 def generate_quine():
@@ -43,9 +64,15 @@ def quine():
 
 
 def main():
-    test_other_python_quines()
-    test_self()
-    quine()
+    output = io.StringIO()
+    with contextlib.redirect_stderr(output):
+        unittest.main()
+    output = output.getvalue()
+
+    if "FAILED" in output:
+        print(output)
+    else:
+        quine()
 
 
 if __name__ == "__main__":
