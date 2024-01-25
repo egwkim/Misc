f0e61e7ecb65fb4ae8132535cfbf2bc9d5a453d6
Geonu Kim <35051914+egwkim@users.noreply.github.com>
2024-01-25 16:28:22 +0900

    Update sphere.py

--- 2024/01/sphere.py
+++ 2024/01/sphere.py
@@ -8,26 +8,26 @@ light_long = 0
 chars = ".,-~:;=!*#$@"
 
 height = 30
-width = 2*height
+width = 2 * height
 r = 15
 
 while 1:
     o = ""
-    for y in (i-(height-1)/2 for i in range(height)):
-        for x in ((i/2-(width-1)/4 for i in range(width))):
+    for y in (i - (height - 1) / 2 for i in range(height)):
+        for x in (i / 2 - (width - 1) / 4 for i in range(width)):
             if x**2 + y**2 > r**2:
                 o += " "
             else:
                 z = sqrt(r**2 - x**2 - y**2)
-                b = z * (
+                b = (
                     x * cos(light_alt) * sin(light_long)
                     + y * sin(light_alt)
                     + z * cos(light_alt) * cos(light_long)
-                ) / r**2
-                b = max(b,0)
-                o += chars[int(b*len(chars))]
+                ) / r
+                b = max(b, 0)
+                o += chars[round(b * (len(chars) - 1))]
         o += "\n"
 
     print(o)
     light_long += 0.01
-    light_alt += 0.002
+    light_alt += 0.0021
