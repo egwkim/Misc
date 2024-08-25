135656ec3b5d6fcf498128a37a9987321e803884
Geonu Kim <35051914+egwkim@users.noreply.github.com>
2024-08-25 17:09:50 +0900

    Save maze as bmp file

--- 2024/08/maze/maze.py
+++ 2024/08/maze/maze.py
@@ -1,7 +1,66 @@
 import random
+import struct
+
+
+def bmp(maze):
+    maze_width = maze[0].bit_length()
+    maze_height = len(maze)
+
+    cell_size = 1
+
+    # maze_width * cell_size pixel per row
+    # maze_width * cell_size bits per row
+    # maze_width * cell_size + 7 // 8 bytes per row
+    # ((maze_width * cell_size + 7 // 8) + 3) // 4
+    row_size = 4 * (((maze_width * cell_size + 7) // 8 + 3) // 4)
+
+    BMP_HEADER_SIZE = 14
+    DIB_HEADER_SIZE = 40
+    PALLETE_SIZE = 4 * 2
+    BMP_DATA_SIZE = row_size * maze_height * cell_size
+    FILE_SIZE = BMP_HEADER_SIZE + DIB_HEADER_SIZE + PALLETE_SIZE + BMP_DATA_SIZE
+
+    # Bitmap file header
+    data = b'BM'  # ID
+    data += struct.pack('<I', FILE_SIZE)  # File size
+    data += struct.pack('<h', 0)  # Unused
+    data += struct.pack('<h', 0)  # Unused
+    data += struct.pack('<I', FILE_SIZE - BMP_DATA_SIZE)  # Start of bitmap data
+
+    # DIB header (BITMAPINFOHEADER)
+    data += struct.pack('<I', 40)  # DIB header size
+    data += struct.pack('<I', maze_width * cell_size)  # Image width
+    data += struct.pack('<I', maze_height * cell_size)  # Image height
+    data += struct.pack('<h', 1)  # Number of color planes
+    data += struct.pack('<h', 1)  # Bits per pixel
+    data += struct.pack('<I', 0)  # Compression method
+    data += struct.pack('<I', BMP_DATA_SIZE)  # Size of bitmap data
+    data += struct.pack('<I', 3780)  # Horizontal resolution, pixel per meter
+    data += struct.pack('<I', 3780)  # Vertical resolution, pixel per meter
+    data += struct.pack('<I', 2)  # Number of colors in the color pallete
+    data += struct.pack('<I', 0)  # Number of important colors
+
+    # Color pallete
+    data += bytearray.fromhex('FFFFFF00')  # White
+    data += bytearray.fromhex('00000000')  # Black
+
+    # Bitmap data
+    for row in maze:
+        maze_data = 0
+        walls = (1 << cell_size) - 1
+        for _ in range(maze_width):
+            if row & 1:
+                maze_data += walls
+            walls <<= cell_size
+            row >>= 1
+        maze_data <<= row_size * 8 - cell_size * maze_width
+        data += maze_data.to_bytes(row_size) * cell_size
+
+    return data
 
 
 def rand_dfs(x, y):
+    # x is inverted
     # 0: shaft, 1: wall
     maze = [2 ** (2 * x + 1) - 1] * (2 * y + 1)
 
@@ -42,6 +101,10 @@ def rand_dfs(x, y):
         maze[2 * cur_y + 1] -= 1 << 2 * cur_x + 1
         visited[cur_y][cur_x] = True
 
+    return maze
+
+
+def maze_str(maze):
     maze_str = '\n'.join(f'{row:b}'[::-1] for row in maze)
     maze_str = maze_str.replace('0', '  ')
     maze_str = maze_str.replace('1', '██')
@@ -50,9 +113,14 @@ def rand_dfs(x, y):
 
 def main():
     x, y = map(int, input().split())
+
     maze = rand_dfs(x, y)
-    with open('maze.txt', 'w', encoding='utf-8') as f:
-        f.write(maze)
+    # with open('maze.txt', 'w', encoding='utf-8') as f:
+    #     f.write(maze_str(maze))
+
+    maze_data = bmp(maze)
+    with open('maze.bmp', 'wb') as f:
+        f.write(maze_data)
 
 
 if __name__ == "__main__":
